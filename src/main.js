628fb0ee74d19b07995fb39e1f847f8131906ed5
Sam Arbid <sam.arbid@capcito.com>
2020-04-07 16:30:45 +0200

    Fix the 404

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import StoryblokVue from 'storyblok-vue'
 import Page from './components/Page'
 import Teaser from '@/components/Teaser'
+import Grid from '@/components/Grid'
 
 Vue.component('page', Page)
 Vue.component('teaser', Teaser)
+Vue.component('grid', Grid)
 
 Vue.use(StoryblokVue)
 Vue.config.productionTip = false
