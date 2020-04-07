5ffeede8c589b0ee2376b12a557d60ff4a788c2b
Sam Arbid <sam.arbid@capcito.com>
2020-04-07 13:26:55 +0200

    setup base

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import StoryblokVue from 'storyblok-vue'
+import Page from './components/Page'
+import Teaser from '@/components/Teaser'
 
+Vue.component('page', Page)
+Vue.component('teaser', Teaser)
+
+Vue.use(StoryblokVue)
 Vue.config.productionTip = false
 
 new Vue({
